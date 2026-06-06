f63a867185582f08c75005d0ff16a09ca5660b7a
hlalefanglesego-debug <hlalefanglesego@gmail.com>
2026-06-06 11:24:35 +0200

    Add variables and arithmetic operations in variables.js
    Initialize variables and perform basic arithmetic operations.

---
+++ variables.js
@@ -0,0 +1,28 @@
+let name = "Hlalefang";
+let age = 25;
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+
+let favouriteColour = "Blue";
+
+console.log("Favourite colour:", favouriteColour);
+
+favouriteColour = "Black";
+
+console.log("New favourite colour:", favouriteColour);
+
+let num1 = 20;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
