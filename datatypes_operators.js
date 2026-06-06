a45e48c4e78aa00c34a1bedf421287a2044a0738
hlalefanglesego-debug <hlalefanglesego@gmail.com>
2026-06-06 11:25:52 +0200

    Add datatypes_operators.js with basic operations

---
+++ datatypes_operators.js
@@ -0,0 +1,53 @@
+let integerNum = 10;
+let floatNum = 5.5;
+
+console.log(integerNum + 2);
+console.log(integerNum - 2);
+console.log(integerNum * 2);
+console.log(integerNum / 2);
+console.log(integerNum % 3);
+console.log(integerNum ** 2);
+
+console.log(integerNum);
+console.log(floatNum);
+
+let numA = 15;
+let numB = 20;
+
+let greater = numA > numB;
+let smaller = numA < numB;
+
+console.log(greater);
+console.log(smaller);
+
+let x = 8;
+let y = 12;
+
+console.log(x > y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x != y);
+
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+let p = 10;
+
+p += 5;
+console.log(p);
+
+p -= 2;
+console.log(p);
+
+p *= 2;
+console.log(p);
+
+p /= 3;
+console.log(p);
+
+p %= 4;
+console.log(p);
